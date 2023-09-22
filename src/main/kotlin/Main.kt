960b370656724addb6608f014f2a428f1ae90d8f
Aksel Holbek Sørbye <akselholbek.sorbye@statnett.no>
2023-09-22 16:44:02 +0200

    Fix Kotlin setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,11 @@
-fun readStrings() = readln().split(" ")
-fun readInt() = readln().toInt()
-fun readInts() = readStrings().map { it.toInt() }
-fun readLong() = readln().toLong()
-fun readLongs() = readStrings().map { it.toLong() }
-fun readDouble() = readln().toDouble()
-fun readDoubles() = readStrings().map { it.toDouble() }
-
-fun solve(line:String): String {
-    return ""
+fun solve(n: Int): Int {
+    return 0
 }
 
 fun main() {
-    val n = 1 //readInt()
-    repeat (n) {
-        val line = readln()
-        val ans = solve(line)
+    while (true) {
+        val n = readlnOrNull()?.toInt() ?: break
+        val ans = solve(n)
         println(ans)
     }
 }
